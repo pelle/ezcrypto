ee798c2633ac8eb494d4ca55395baa435debea36
Pelle Braendgaard <pelleb@gmail.com>
2006-02-25 12:49:02 +0000

    Adding Dirk's file encryption routines.

--- lib/ezcrypto.rb
+++ lib/ezcrypto.rb
@@ -41,8 +41,26 @@ https://stakeitout.com
 =end
 
   class Key
-    attr_reader :raw,:algorithm
-    
+
+    attr_reader :raw, :algorithm
+
+    @@block_size = 512
+
+=begin rdoc
+Set the block-size for IO-operations (default: 512 bytes)
+=end
+    def self.block_size=(size)
+      @@block_size = size
+    end
+
+=begin rdoc
+Return the block-size for IO-operations.
+=end
+    def self.block_size
+      @@block_size
+    end
+
+
 =begin rdoc
 Initialize the key with raw unencoded binary key data. This needs to be at least
 16 bytes long for the default aes-128 algorithm.
@@ -51,28 +69,28 @@ Initialize the key with raw unencoded binary key data. This needs to be at least
       @raw=raw
       @algorithm=options[:algorithm]||"aes-128-cbc"
     end
-    
+
 =begin rdoc
 Generate random key.
 =end
-    def self.generate(options = {})      
+    def self.generate(options = {})
       Key.new(EzCrypto::Digester.generate_key(calculate_key_size(options[:algorithm])),options)
     end
-            
+
 =begin rdoc
-Create key generated from the given password and salt  
+Create key generated from the given password and salt
 =end
     def self.with_password(password,salt,options = {})
       Key.new(EzCrypto::Digester.get_key(password,salt,calculate_key_size(options[:algorithm])),options)
     end
-    
+
 =begin rdoc
 Initialize the key with Base64 encoded key data.
 =end
     def self.decode(encoded,options = {})
       Key.new(Base64.decode64(encoded),options)
     end
-    
+
 =begin rdoc
 Encrypts the data with the given password and a salt. Short hand for:
 
@@ -84,30 +102,31 @@ Encrypts the data with the given password and a salt. Short hand for:
       key=Key.with_password(password,salt,options)
       key.encrypt(data)
     end
-    
+
 =begin rdoc
 Decrypts the data with the given password and a salt. Short hand for:
 
   key=Key.with_password(password,salt,options)
   key.decrypt(data)
 
-  
+
 =end
     def self.decrypt_with_password(password,salt,data,options = {})
       key=Key.with_password(password,salt,options)
       key.decrypt(data)
     end
-    
+
 =begin rdoc
-Given an algorithm this calculates the keysize. This is used by both the generate and with_password methods. This is not yet 100% complete.  
+Given an algorithm this calculates the keysize. This is used by both
+the generate and with_password methods. This is not yet 100% complete.
 =end
-    def self.calculate_key_size(algorithm)    
+    def self.calculate_key_size(algorithm)
       if !algorithm.nil?
         algorithm=~/^([[:alnum:]]+)(-(\d+))?/
         if $3
           size=($3.to_i)/8
-        else 
-          case $1 
+        else
+          case $1
             when "bf"
               size = 16
             when "blowfish"
@@ -126,7 +145,7 @@ Given an algorithm this calculates the keysize. This is used by both the generat
               size = 16
             when "rc4"
               size = 16
-            else 
+            else
               size = 16
             end
         end
@@ -134,7 +153,7 @@ Given an algorithm this calculates the keysize. This is used by both the generat
       if size.nil?
         size = 16
       end
-      
+
       size
     end
 
@@ -142,27 +161,27 @@ Given an algorithm this calculates the keysize. This is used by both the generat
 returns the Base64 encoded key.
 =end
     def encode
-      Base64.encode64(@raw).chop
+      Base64.encode64 @raw
     end
-    
+
 =begin rdoc
-returns the Base64 encoded key. Synonym for encode.  
+returns the Base64 encoded key. Synonym for encode.
 =end
     def to_s
       encode
     end
-    
+
 =begin rdoc
 Encrypts the data and returns it in encrypted binary form.
 =end
-  def encrypt(data)
-    if data==nil || data==""
-      nil
-    else  
-      @cipher=EzCrypto::Encrypter.new(self,"",@algorithm)
-      @cipher.encrypt(data)
+    def encrypt(data)
+      if data==nil || data==""
+        nil
+      else
+        encrypter("")
+        @cipher.encrypt(data)
+      end
     end
-  end
 
 =begin rdoc
 Encrypts the data and returns it in encrypted Base64 encoded form.
@@ -170,27 +189,215 @@ Encrypts the data and returns it in encrypted Base64 encoded form.
     def encrypt64(data)
       Base64.encode64(encrypt(data))
     end
-    
+
 =begin rdoc
 Decrypts the data passed to it in binary format.
-=end    
+=end
     def decrypt(data)
-      @cipher=EzCrypto::Decrypter.new(self,"",@algorithm)
-      @cipher.gulp(data)
+      if data==nil || data==""
+        nil
+      else
+        decrypter("")
+        @cipher.gulp(data)
+      end
     rescue
       puts @algorithm
       throw $!
     end
-    
+
 =begin rdoc
-Decrypts a Base64 formatted string  
+Decrypts a Base64 formatted string
 =end
     def decrypt64(data)
       decrypt(Base64.decode64(data))
     end
 
+
+=begin rdoc
+Create a file with minimal permissions, and yield
+it to a block. By default only the owner may read it.
+=end
+    def safe_create(file, mod=0400, mask=0066)
+      begin
+        old_umask = File.umask
+        File.umask(mask)
+        File.open(file, File::CREAT | File::EXCL | File::WRONLY) do |f|
+          yield(f) if block_given?
+        end
+      ensure
+        File.umask(old_umask)
+      end
+      File.chmod(mod, file) if File.exists?(file)
+      File.size(file)
+    end
+
+
+=begin rdoc
+Overwrite a file with zero values before removing it's filesystem inode.
+This is not very safe :-)
+=end
+    def safe_delete(file)
+      begin
+        to_clear = File.size(file)
+        zeroes   = Array.new(Key.block_size, "\0").join
+        File.chmod(0600, file)
+        File.open(file, File::WRONLY) do |f|
+          f.rewind
+          while to_clear > 0
+            f.write(zeroes)
+            to_clear -= Key.block_size
+          end
+          f.flush
+        end
+      ensure
+        File.delete(file) if File.exists?(file)
+      end
+      return !File.exists?(file)
+    end
+
+
+=begin rdoc
+Open a file readonly and yield it to a block.
+=end
+    def safe_read(file)
+      File.open(file, File::RDONLY) do |i|
+        yield(i) if block_given?
+      end
+    end
+
+    private :safe_create, :safe_read, :safe_delete
+
+=begin rdoc
+Load a key from a yaml_file generated via Key#store.
+=end
+    def self.load(filename)
+      require 'yaml'
+      hash = YAML::load_file(filename)
+      req = proc { |k| hash[k] or raise "Missing element #{k} in #{filename}" }
+      key = self.new Base64.decode64(req.call(:key)) , :algorithm => req.call(:algorithm)
+      return key
+    end
+
+
+=begin rdoc
+Save the key data into a file, try to do this in a secure manner.
+NOTE: YAML::store & friends are not used to encance control over
+the generated file format.
+=end
+    def store(filename)
+      safe_create(filename) do |f|
+        selfenc = self.encode
+        f.puts "---"
+        f.puts ":EZCRYPTO KEY FILE: KEEP THIS SECURE !"
+        f.puts ":created: #{Time.now}"
+        f.puts ":algorithm: #{@algorithm}"
+        f.puts ":key: #{selfenc}"
+      end
+    end
+
+
+=begin rdoc
+Get a Encrypter object. You have to call #final on it by yourself!
+=end
+    def encrypter(target='')
+      @cipher = EzCrypto::Encrypter.new(self,target,@algorithm)
+    end
+
+
+=begin rdoc
+Get a Decrypter object. You have to call #final on it by yourself!
+=end
+    def decrypter(target='')
+      @cipher = EzCrypto::Decrypter.new(self,target,@algorithm)
+    end
+
+=begin rdoc
+Create a Decrypter object and yield it to a block.
+You must *not* call #final by yourself, the method
+does this.
+=end
+    def on_decrypter(target='', &block)
+      decrypter(target)
+      on_cipher(&block)
+    end
+
+=begin rdoc
+Create an Encrypter object and yield it to a block.
+You must *not* call #final by yourself, the method
+does this.
+=end
+    def on_encrypter(target='', &block)
+      encrypter(target)
+      on_cipher(&block)
+    end
+
+
+=begin rdoc
+Helper method, yields the current cipher to a block.
+=end
+    def on_cipher(&block)
+      begin
+        block.call @cipher
+      ensure
+        @cipher.final
+      end
+    end
+
+    private :on_cipher
+
+
+=begin rdoc
+Encrypt a file 'inplace' and add a suffix
+see #cipher_file.
+IMPORTANT: The inputfile will be deleted by default.
+=end
+    def encrypt_file(src, tgt=nil, options = {} )
+      options = { :suffix => '.ez', :autoclean => 'true' }.update(options)
+      tgt = "#{src}#{options[:suffix]}" unless tgt
+      cipher_file :on_encrypter, src, tgt, options[:autoclean]
+    end
+
+
+=begin rdoc
+Decrypt a file 'inplace' and remove a suffix
+see #cipher_file
+IMPORTANT: The inputfile will be deleted by default.
+=end
+    def decrypt_file(src, tgt=nil, options = {} )
+      options = { :suffix => '.ez', :autoclean => 'true' }.update(options)
+      unless tgt
+        tgt = src
+        tgt = tgt.gsub(/#{options[:suffix]}$/, '')
+      end
+      cipher_file :on_decrypter, src, tgt, options[:autoclean]
+    end
+
+
+=begin rdoc
+uses either  #on_decrypter or #on_encrypter to transform a given sourcefile
+to a targetfile. the sourcefile is deleted after sucessful transformation
+the delete_source is 'true'.
+=end
+    def cipher_file(method, sourcefile, targetfile, delete_source)
+      raise(ArgumentError, "source == target #{sourcefile}") if sourcefile == targetfile
+      safe_create(targetfile,0600) do |o|
+        self.send(method, o) do |c|
+          safe_read(sourcefile) do |i|
+            loop do
+              buffer = i.read(Key.block_size) or break
+              c << buffer
+            end
+          end
+        end
+      end
+      safe_delete(sourcefile) if delete_source && File.exists?(targetfile)
+      return targetfile
+    end
+    
+    private :cipher_file
     
   end
+  
 =begin rdoc
 Abstract Wrapper around OpenSSL's Cipher object. Extended by Encrypter and Decrypter.
   
@@ -217,12 +424,17 @@ Warning! The interface may change.
       @finished=false
     end
     
+    
+    def to_target(data)
+      return data
+    end
+    
 =begin rdoc
 Process the givend data with the cipher.
 =end
     def update(data)
       reset if @finished
-      @target<< @cipher.update(data)
+      @target<< to_target(@cipher.update(data))
     end
 
 =begin rdoc
@@ -236,7 +448,7 @@ Process the givend data with the cipher.
 Finishes up any last bits of data in the cipher and returns the final result.
 =end
     def final
-      @target<< @cipher.final
+      @target<< to_target(@cipher.final)
       @finished=true
       @target
     end
--- test/ezcrypto_test.rb
+++ test/ezcrypto_test.rb
@@ -1,6 +1,7 @@
 $:.unshift(File.dirname(__FILE__) + "/../lib/")
 
 require 'test/unit'
+require 'fileutils'
 require 'ezcrypto'
 require 'base64'
 
@@ -24,17 +25,17 @@ class EzCryptoTest < Test::Unit::TestCase
       assert_with_password "","secret","aes-128-cbc",16
       assert_with_password "test","secret","aes-128-cbc",16
       assert_with_password "password","secret","aes-128-cbc",16
-      assert_with_password "aæsldfad8q5æ34j2æl4j24l6j2456","secret","aes-128-cbc",16
+      assert_with_password "aï¿½sldfad8q5ï¿½34j2ï¿½l4j24l6j2456","secret","aes-128-cbc",16
       
       assert_with_password "","secret","aes-192-cbc",24
       assert_with_password "test","secret","aes-192-cbc",24
       assert_with_password "password","secret","aes-192-cbc",24
-      assert_with_password "aæsldfad8q5æ34j2æl4j24l6j2456","secret","aes-192-cbc",24
+      assert_with_password "aï¿½sldfad8q5ï¿½34j2ï¿½l4j24l6j2456","secret","aes-192-cbc",24
 
       assert_with_password "","secret","aes-256-cbc",32
       assert_with_password "test","secret","aes-256-cbc",32
       assert_with_password "password","secret","aes-256-cbc",32
-      assert_with_password "aæsldfad8q5æ34j2æl4j24l6j2456","secret","aes-256-cbc",32
+      assert_with_password "aï¿½sldfad8q5ï¿½34j2ï¿½l4j24l6j2456","secret","aes-256-cbc",32
 
   end
 
@@ -50,8 +51,7 @@ class EzCryptoTest < Test::Unit::TestCase
     end  
   end
   
-  def test_decrypt
-    
+  def test_decrypt    
     0.upto(CLEAR_TEXT.size) do |size|
       assert_decrypt CLEAR_TEXT[0..size]
     end  
@@ -62,7 +62,104 @@ class EzCryptoTest < Test::Unit::TestCase
       assert_decrypt64 CLEAR_TEXT[0..size]
     end  
   end
+    
+  def test_keyfile_store_load
 
+    algo, size = "aes-256-cbc", 32   
+    keyfile = 'ezcrypto-test.key'
+    
+    FileUtils.rm [keyfile], :force => true
+    key = EzCrypto::Key.generate :algorithm => algo    
+    assert_file_not_exists keyfile    
+    key.store keyfile    
+    assert_file_exists keyfile    
+    assert_file_permissions keyfile, 0100400    
+    
+    key2 = EzCrypto::Key.load(keyfile)    
+    assert_equal key.raw, key2.raw
+    
+    FileUtils.rm [keyfile], :force => true    
+  end
+  
+  def test_filestuff_with_defaults      
+
+    clearfile = 'lorem_ipsum.txt'
+    keyfile = 'lorem_ipsum.key'
+    algo, size = "aes-256-cbc", 32
+    
+    File.open(clearfile, 'w') { |f| f.write(CLEAR_TEXT) }
+    assert_file_contains clearfile, CLEAR_TEXT    
+
+    key = EzCrypto::Key.generate :algorithm => algo    
+
+    # default behaviour: remove clearfile, append '.ez' suffix  
+    cryptfile = key.encrypt_file(clearfile)    
+    assert_equal cryptfile, clearfile + ".ez"
+    assert_file_not_exists clearfile
+    assert_file_exists cryptfile 
+    assert_file_contains cryptfile, key.encrypt(CLEAR_TEXT)    
+
+    # default behaviour: unlink cryptfile and remove suffix from filename
+    clearfile = key.decrypt_file cryptfile
+    assert_file_exists clearfile
+    assert_file_not_exists cryptfile
+    assert_file_contains clearfile, CLEAR_TEXT    
+    FileUtils.rm [keyfile, clearfile, cryptfile], :force => true
+  end
+  
+  def test_filestuff_with_options
+  
+    clearfile = 'lorem_ipsum.txt'
+    keyfile = 'lorem_ipsum.key'
+    algo, size = "aes-256-cbc", 32    
+
+    File.open(clearfile, 'w') { |f| f.write(CLEAR_TEXT) }
+    assert_file_contains clearfile, CLEAR_TEXT
+    
+    key = EzCrypto::Key.generate :algorithm => algo      
+    
+    # with options: keep the original file, auto-create a 
+    # new filename with a user-defined suffix        
+    cryptfile = key.encrypt_file(clearfile, nil, :autoclean => false, :suffix => '.Encrypted')
+    assert_equal cryptfile, clearfile + ".Encrypted", 'suffix was added'
+    assert_file_exists clearfile
+    assert_file_exists cryptfile    
+    assert_file_permissions cryptfile, 0100600
+    
+    assert_raises(Errno::EEXIST, "the original file would not be overwritten") { 
+      key.decrypt_file(cryptfile, nil, :autoclean => false, :suffix => '.Encrypted')
+    } 
+    FileUtils.rm [clearfile], :force => true    
+
+    clearfile = key.decrypt_file(cryptfile, nil, :autoclean => false, :suffix => '.Encrypted')
+    assert_equal cryptfile, clearfile + ".Encrypted", 'suffix was removed'    
+    assert_file_exists clearfile
+    assert_file_exists cryptfile
+    assert_file_permissions cryptfile, 0100600    
+    assert_file_contains clearfile, CLEAR_TEXT    
+
+    FileUtils.rm [keyfile, clearfile, cryptfile], :force => true        
+  end
+    
+  def assert_file_permissions(filename, mode, msg='')
+    fmode = File.stat(filename).mode
+    assert_equal fmode, mode, msg 
+  end
+  
+  def assert_file_exists(filename)
+    assert File.exists?(filename)
+  end
+
+  def assert_file_not_exists(filename)
+    assert !File.exists?(filename)
+  end
+  
+  def assert_file_contains(filename, expected)
+    assert_file_exists(filename)
+    content = File.open(filename,'r').read
+    assert_equal expected, content
+  end
+  
   def assert_key_size(size,key)
     assert_equal size,key.raw.size      
   end
@@ -100,13 +197,16 @@ class EzCryptoTest < Test::Unit::TestCase
       assert_equal clear,key.decrypt(encrypted)
     end
   end
+  
   def assert_decrypt64(clear)
     key=EzCrypto::Key.generate
     encrypted=key.encrypt64 clear
     assert_not_nil encrypted
     assert_equal clear,key.decrypt64(encrypted)
   end
+  
   ALGORITHMS=["aes128","bf","blowfish","des","des3","rc4","rc2"]
   CLEAR_TEXT="Lorem ipsum dolor sit amet, suspendisse id interdum mus leo id. Sapien tempus consequat nullam, platea vitae sociis sed elementum et fermentum, vel praesent eget. Sed blandit augue, molestie mus sed habitant, semper voluptatibus neque, nullam a augue. Aptent imperdiet curabitur, quam quis laoreet. Dolor magna. Quis vestibulum amet eu arcu fringilla nibh, mi urna sunt dictumst nulla, elit quisque purus eros, sem hendrerit. Vulputate tortor rhoncus ac nonummy tortor nulla. Nunc id nunc luctus ligula."
 end
+
 
