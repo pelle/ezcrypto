c61b6d19ae21d519db9324ad050c4ccdd92817c4
Pelle Braendgaard <pelleb@gmail.com>
2006-07-31 23:22:29 +0000

    Improved support for X509 certificates.

--- lib/ezsig.rb
+++ lib/ezsig.rb
@@ -45,13 +45,25 @@ module EzCrypto
     end
   
     def self.decode(encoded)
-    
+      case encoded
+      when /-----BEGIN PUBLIC KEY-----/
+        EzCrypto::Verifier.new(OpenSSL::PKey::RSA.new( encoded))
+      when /-----BEGIN CERTIFICATE-----/
+        EzCrypto::Certificate.new(OpenSSL::X509::Certificate.new( encoded))
+      else
+        raise "Unknown public key format"
+      end
     end
   
-    def self.from_file(file)
-    
+    def self.from_file(filename)
+      file = File.read( filename )
+      decode(file)
     end
   
+    def cert?
+      false
+    end
+        
     def public_key
       @pub
     end
@@ -60,4 +72,99 @@ module EzCrypto
       @pub.verify( OpenSSL::Digest::SHA1.new, sig, data )
     end
   end
+  
+  class Certificate < Verifier
+    
+    def initialize(cert)
+      super(cert.public_key)
+      @cert=cert
+    end
+    
+    def cert?
+      true
+    end
+    
+    def subject
+      @cert.subject
+    end
+    
+    def issuer
+      @cert.issuer
+    end
+    
+    def serial
+      @cert.serial
+    end
+    
+    def cert
+      @cert
+    end
+    
+    def not_before
+      @cert.not_before
+    end
+    
+    def not_after
+      @cert.not_after
+    end
+    
+    def valid?(time=Time.now.utc)
+      time.to_i>self.not_before.to_i && time.to_i<self.not_after.to_i
+    end
+    
+    def email
+      self[:emailAddress]
+    end
+
+    def country
+      self[:C]
+    end
+    alias_method :c,:country
+
+    def state
+      self[:ST]
+    end
+    alias_method :st,:state
+    
+    def locality
+      self[:L]
+    end
+    alias_method :l,:locality
+    
+    def organizational_unit
+      self[:OU]
+    end
+    alias_method :ou,:organizational_unit
+    alias_method :organisational_unit,:organizational_unit
+    
+    def organization
+      self[:O]
+    end
+    alias_method :o,:organization
+    alias_method :organisation,:organization
+    
+    def common_name
+      self[:CN]
+    end
+    alias_method :name,:common_name
+    alias_method :cn,:common_name
+    
+    def [](attr_key)
+      unless @attributes
+        @attributes={}
+        subject.to_s.split(/\//).each do |field| 
+          key, val = field.split(/=/,2)
+          if key
+            @attributes[key.to_sym]=val
+          end
+        end
+      end
+      @attributes[attr_key.to_sym]
+    end
+    
+    def method_missing(method)
+      self[method]
+    end
+  end
+  
 end
--- test/dsig_test.rb
+++ test/dsig_test.rb
@@ -25,6 +25,65 @@ class EzCryptoTest < Test::Unit::TestCase
     assert_signer(signer)
   end
   
+  def test_public_key_read
+    signer=EzCrypto::Signer.from_file "testsigner.pem"
+    verifier=EzCrypto::Verifier.from_file "testpub.pem"
+    assert verifier
+    assert !verifier.cert?
+    assert_equal signer.public_key.to_s, verifier.public_key.to_s
+  end
+  
+  def test_certificate_reader
+    signer=EzCrypto::Signer.from_file "testsigner.pem"
+    cert=EzCrypto::Verifier.from_file "testsigner.cert"
+    assert cert
+    assert cert.cert?
+    assert_instance_of EzCrypto::Certificate, cert
+    assert_equal signer.public_key.to_s, cert.public_key.to_s
+    assert_equal "/C=DK/ST=Denmark/L=Copenhagen/O=EzCrypto Test Certificate/OU=testing/CN=EzCrypto Testing/emailAddress=pelleb@gmail.com",cert.subject.to_s
+    assert_equal "/C=DK/ST=Denmark/L=Copenhagen/O=EzCrypto Test Certificate/OU=testing/CN=EzCrypto Testing/emailAddress=pelleb@gmail.com",cert.issuer.to_s
+    
+    assert cert.serial
+    assert cert.not_after
+    assert cert.not_before
+    assert cert.valid?
+    
+    assert_equal cert[:emailAddress],"pelleb@gmail.com"
+    assert_equal cert[:C],"DK"
+    assert_equal cert[:ST],"Denmark"
+    assert_equal cert[:L],"Copenhagen"
+    assert_equal cert[:OU],"testing"
+    assert_equal cert[:O],"EzCrypto Test Certificate"
+    assert_equal cert[:CN],"EzCrypto Testing"
+    
+    assert_equal cert.emailAddress,"pelleb@gmail.com"
+    assert_equal cert.C,"DK"
+    assert_equal cert.ST,"Denmark"
+    assert_equal cert.L,"Copenhagen"
+    assert_equal cert.OU,"testing"
+    assert_equal cert.O,"EzCrypto Test Certificate"
+    assert_equal cert.CN,"EzCrypto Testing"
+    
+    assert_equal cert.email,"pelleb@gmail.com"
+    assert_equal cert.c,"DK"
+    assert_equal cert.st,"Denmark"
+    assert_equal cert.l,"Copenhagen"
+    assert_equal cert.ou,"testing"
+    assert_equal cert.o,"EzCrypto Test Certificate"
+    assert_equal cert.cn,"EzCrypto Testing"
+    
+    assert_equal cert.country,"DK"
+    assert_equal cert.state,"Denmark"
+    assert_equal cert.locality,"Copenhagen"
+    assert_equal cert.organisational_unit,"testing"
+    assert_equal cert.organisation,"EzCrypto Test Certificate"
+    assert_equal cert.organizational_unit,"testing"
+    assert_equal cert.organization,"EzCrypto Test Certificate"
+    assert_equal cert.name,"EzCrypto Testing"
+    assert_equal cert.common_name,"EzCrypto Testing"
+    
+  end
+  
   def assert_signer(signer)
     assert signer
     assert signer.public_key
